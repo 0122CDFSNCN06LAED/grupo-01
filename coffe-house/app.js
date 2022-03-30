03c5807fdd9a892fc6974be307cacc36da07db9a
RubenVen <venruben21@gmail.com>
2022-03-29 21:37:06 -0300

    Modifique app.js-linea-28 y agrego 2 archivos css y un archivo html

--- coffe-house/app.js
+++ coffe-house/app.js
@@ -1,27 +1,30 @@
-const express = require ("express");
+const express = require("express");
 const app = express();
-const path = require ("path");
+const path = require("path");
 
 //le damos acceso a carpeta public
 app.use(express.static(path.join(__dirname, "public")));
 //Levantar el servidor
 app.listen(3000, () => {
-    console.log("servidor corriendo http://localhost:3000/")
-});//el link me permite acceder rápidamente desde la terminal
+  console.log("servidor corriendo http://localhost:3000/");
+}); //el link me permite acceder rápidamente desde la terminal
 
 //creamos las rutas
-app.get("/", (req,res) => {
-    res.sendFile(path.join(__dirname, "views/home.html"))
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "views/home.html"));
 });
-app.get("/product", (req,res) => {
-    res.sendFile(path.join(__dirname, "views/product-detail.html"))
+app.get("/product", (req, res) => {
+  res.sendFile(path.join(__dirname, "views/product-detail.html"));
 });
-app.get("/login", (req,res) => {
-    res.sendFile(path.join(__dirname, "views/login.html"))
+app.get("/login", (req, res) => {
+  res.sendFile(path.join(__dirname, "views/login.html"));
 });
-app.get("/register", (req,res) => {
-    res.sendFile(path.join(__dirname, "views/register.html"))
+app.get("/register", (req, res) => {
+  res.sendFile(path.join(__dirname, "views/register.html"));
 });
-app.get("/cart", (req,res) => {
-    res.sendFile(path.join(__dirname, "views/cart.html"))
+app.get("/cart", (req, res) => {
+  res.sendFile(path.join(__dirname, "views/cart.html"));
+});
+app.get("/shopping-cart", (req, res) => {
+  res.sendFile(path.join(__dirname, "views/shopping-cart.html"));
 });
