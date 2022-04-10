dc057e63e1569868835dc0290263e3af2f0ef3c3
raulo2032 <100073460+raulo2032@users.noreply.github.com>
2022-04-10 20:48:30 -0300

    creo loginController para modelo MVC

---
+++ coffe-house/src/controllers/loginController.js
@@ -0,0 +1,9 @@
+const path = require ("path")
+
+const loginController = {
+    login: (req, res) => {
+        res.sendFile(path.join(__dirname, '../views/login.html'))
+    }
+}
+
+module.exports = loginController;
