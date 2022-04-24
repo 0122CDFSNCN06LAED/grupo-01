26165542a72554cfc4519f69ce98be7c96aa5343
RubenVen <venruben21@gmail.com>
2022-04-23 21:24:23 -0300

    Agrego rutas a editar, borrar y sus metodos

--- coffe-house/src/controllers/productController.js
+++ coffe-house/src/controllers/productController.js
@@ -1,84 +1,146 @@
-const path = require ("path");
-const fs = require ("fs");
-const productsFilePath = path.join(__dirname, '../data/productsDataBase.json')
-const products = JSON.parse(fs.readFileSync(productsFilePath, 'utf-8'));
+const path = require("path");
+const fs = require("fs");
+const productsFilePath = path.join(__dirname, "../data/productsDataBase.json");
+const products = JSON.parse(fs.readFileSync(productsFilePath, "utf-8"));
 
 const productController = {
-    listar: (req, res) => {
-      const blonde= products.filter((p) => p.category == "blonde");
-		const medium = products.filter((p) => p.category == "medium");
+  listar: (req, res) => {
+    const blonde = products.filter((p) => p.category == "blonde");
+    const medium = products.filter((p) => p.category == "medium");
     const dark = products.filter((p) => p.category == "dark");
-      res.render("products/products-index", {style: 'products-index.css', title: 'Create product', 
+    res.render("products/products-index", {
+      style: "products-index.css",
+      title: "Create product",
       blonde: blonde,
-    medium:medium,
-  dark:dark})
-    },
+      medium: medium,
+      dark: dark,
+    });
+  },
 
-    product: (req, res) => {
-        const id = req.params.id;
-		const product = products.find((p) => id == p.id);
+  product: (req, res) => {
+    const id = req.params.id;
+    const product = products.find((p) => id == p.id);
 
-		res.render("products/product-detail", {
-			product: product})
-       
-    },
+    res.render("products/product-detail", {
+      product: product,
+    });
+  },
 
-    create: (req, res) => {
-        res.render('products/product-create', {style: 'product-create.css', title: 'Create product'});
-    },
-    store: (req,res) => {
-        const lastIndex = products.length - 1;
-		const lastProduct = products[lastIndex];
-		const biggestId = lastProduct ? lastProduct.id : 0;
-		const newId = biggestId + 1;
-	
-		const product = {
-		  id: newId,
-          name: req.body.name,
-          category: req.body.category,
-          region: req.body.region,
-          description: req.body.description,
-          grind: req.body.grind,
-          weight: req.body.weight,
-          image: req.file.filename,
-          price: req.body.price,
-		};
-	
-		products.push(product);
-	
-		const jsonTxt = JSON.stringify(products, null, 2);
-		fs.writeFileSync(productsFilePath, jsonTxt, "utf-8");
-	
-		res.redirect("/product/detail/" + newId);
-    },
-    edit: (req, res) => {
-        const id = req.params.id;
-	
-		const product = products.find((p) => id == p.id);
-	
-		res.render('products/product-edit', {style: 'product-create.css', title: 'Edit product', product});
-       /*  res.render('products/product-edit', {style: 'product-create.css', title: 'Edit product'}); */
-    },
-    update: (req, res) => {
-		const id = req.params.id;
-	
-		const product = products.find((p) => id == p.id);
-		
-		Object.assign(product, {
-		  ...req.body,
-		  /* price: Number(req.body.price),
-		  weight: Number(req.body.weight), */
-		});
-	
-		const jsonTxt = JSON.stringify(products, null, 2);
-		fs.writeFileSync(productsFilePath, jsonTxt, "utf-8");
-	
-		res.redirect("/products/detail/" + id);
-	  },
-    cart: (req, res) => {
-        /* res.sendFile(path.join(__dirname, 'cart.html')) */
-        res.render('products/cart', {style: 'cart.css', title: 'Cart'});
-    }
-}
+  create: (req, res) => {
+    res.render("products/product-create", {
+      style: "product-create.css",
+      title: "Create product",
+    });
+  },
+  store: (req, res) => {
+    const lastIndex = products.length - 1;
+    const lastProduct = products[lastIndex];
+    const biggestId = lastProduct ? lastProduct.id : 0;
+    const newId = biggestId + 1;
+
+    const product = {
+      id: newId,
+      name: req.body.name,
+      category: req.body.category,
+      region: req.body.region,
+      description: req.body.description,
+      grind: req.body.grind,
+      weight: req.body.weight,
+      image: req.file.filename,
+      price: req.body.price,
+    };
+
+    products.push(product);
+
+    const jsonTxt = JSON.stringify(products, null, 2);
+    fs.writeFileSync(productsFilePath, jsonTxt, "utf-8");
+
+    res.redirect("/product/detail/" + newId);
+  },
+  edit: (req, res) => {
+    const id = req.params.id;
+
+    const product = products.find((p) => id == p.id);
+
+    //CALLBACK DE ARRAY USADO EN PRODUCT-EDIT.EJS, INPUT CHECK GRANOS.
+    const cb = (valor) => {
+      for (const element of product.grind) {
+        if (element === valor) return "checked";
+      }
+    };
+
+    //CALLBACK DE OBJETO LITERAL USADO EN PRODUCT-EDIT.EJS INPUT CHECK GRANOS.
+    const cbObj = (valor) => {
+      if (product.grind === valor) return "checked";
+    };
+
+    //CALLBACK DE ARRAY USADO EN PRODUCT-EDIT.EJS INPUT CHECK PESO.
+    const cbW = (valor) => {
+      for (const element of product.weight) {
+        if (element === valor) return "checked";
+      }
+    };
+
+    //CALLBACK DE OBJETO LITERAL USADO EN PRODUCT-EDIT.EJS INPUT CHECK PESO.
+    const cbOW = (valor) => {
+      if (product.weight === valor) return "checked";
+    };
+
+    res.render("products/product-edit", {
+      style: "product-create.css",
+      title: "Edit product",
+      product,
+      cb,
+      cbObj,
+      cbW,
+      cbOW,
+    });
+    /*  res.render('products/product-edit', {style: 'product-create.css', title: 'Edit product'}); */
+  },
+  update: (req, res) => {
+    const id = req.params.id;
+    const file = req.file;
+
+    const product = products.find((p) => id == p.id);
+
+    //ALAMACENO LOS DATOS EN CASO DE SER MODIFICADOS Y SINO NO MODIFICA LOS DATOS.
+    const prodModif = {
+      ...product,
+      ...req.body,
+      image: file === undefined ? product.image : file.filename,
+    };
+
+    //BUSCO EL INDICE DEL PRODUCTO EN PRODUCTS QUE VIENE DE JSON.
+    const indexProd = products.findIndex((prod) => prod.id == id);
+
+    //REEMPLAZO LOS DATOS DE ESE INDICE POR LOS MODIFICADOS EN CASO DE SER ASI.
+    const insetProduct = products.splice(indexProd, 1, prodModif);
+
+    //GUARDO LOS DATOS EN JSON.
+    const jsonTxt = JSON.stringify(products, null, 2);
+    const productEdit = fs.writeFileSync(productsFilePath, jsonTxt, "utf-8");
+
+    res.redirect("/product/detail/" + id);
+  },
+  destroy: (req, res) => {
+    const id = req.params.id;
+
+    //BUSCO EL INDICE DEL PRODUCTO EN PRODUCTS QUE VIENE DE JSON.
+    const indexProd = products.findIndex((prod) => prod.id == id);
+
+    //REEMPLAZO LOS DATOS DE ESE INDICE POR LOS MODIFICADOS EN CASO DE SER ASI.
+    const insetProduct = products.splice(indexProd, 1);
+
+    //GUARDO LOS DATOS EN JSON.
+    const jsonTxt = JSON.stringify(products, null, 2);
+    const productEdit = fs.writeFileSync(productsFilePath, jsonTxt, "utf-8");
+
+    res.redirect("/");
+  },
+  cart: (req, res) => {
+    /* res.sendFile(path.join(__dirname, 'cart.html')) */
+    res.render("products/cart", { style: "cart.css", title: "Cart" });
+  },
+};
 
 module.exports = productController;
--- coffe-house/src/routes/product.js
+++ coffe-house/src/routes/product.js
@@ -1,30 +1,42 @@
-const express = require('express');
-const productController = require('../controllers/productController');
-const multer = require ('multer');
-const path = require('path');
+const express = require("express");
+const productController = require("../controllers/productController");
+const multer = require("multer");
+const path = require("path");
 
 const storage = multer.diskStorage({
-    destination: (req, file, cb) => {
-        cb (null, path.join(__dirname,'../../public/img/products'))
-    },
-    filename: (req, file, cb) => {
-        const newFilename = 'product-'+ Date.now() + path.extname(file.originalname);
-        cb(null, newFilename);
-    }
+  destination: (req, file, cb) => {
+    cb(null, path.join(__dirname, "../../public/img/products"));
+  },
+  filename: (req, file, cb) => {
+    const newFilename =
+      "product-" + Date.now() + path.extname(file.originalname);
+    cb(null, newFilename);
+  },
 });
 const uploadFile = multer({ storage });
 
 const router = express.Router();
-router.get('/product', productController.listar)
 //VISTA DETALLE
-router.get('/product/detail/:id', productController.product);
+router.get("/product/detail/:id", productController.product);
 //CREACION PRODUCTO
-router.get('/product/create', productController.create);
-router.post('/product/create', uploadFile.single('image'), productController.store);
+router.get("/product/create", productController.create);
+router.post(
+  "/product/create",
+  uploadFile.single("image"),
+  productController.store
+);
 //EDICION PRODUCTO
-router.get('/edit/:id', productController.edit); 
-router.put('/:id', productController.update); 
-//CARRITO
-router.get('/cart', productController.cart);
+router.get("/product/edit/:id", productController.edit);
+router.put(
+  "/edit/:id",
+  uploadFile.single("editImage"),
+  productController.update
+);
 
-module.exports= router;
+//BORRAR PRODUCTO
+router.delete("/product/delete/:id", productController.destroy);
+
+//CARRITO
+router.get("/cart", productController.cart);
+
+module.exports = router;
