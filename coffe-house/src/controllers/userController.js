a430a357f12564a296056364e0e5542026815f26
raulo2032 <100073460+raulo2032@users.noreply.github.com>
2022-04-28 18:58:43 -0300

    Agrego lineas para hacer el post de creacion de usuario

--- coffe-house/src/controllers/userController.js
+++ coffe-house/src/controllers/userController.js
@@ -1,4 +1,7 @@
 const path = require ("path")
+const fs = require("fs");
+const usersFilePath = path.join(__dirname, "../data/usersDataBase.json");
+const users = JSON.parse(fs.readFileSync(usersFilePath, "utf-8"));
 
 const userController = {
     login: (req, res) => {
@@ -7,8 +10,36 @@ const userController = {
     },
     register: (req, res) => {
         /*  res.sendFile(path.join(__dirname, 'register.html')) */
-         res.render('user/register', {style: 'register.css', title: 'Register'});
-     }
+
+        res.render('user/register', {style: 'register.css', title: 'Register'});
+    
+        const lastIndex = users.length - 1;
+        const lastUser = users[lastIndex];
+        const biggestId = lastUser ? lastUser.id : 0;
+        const newId = biggestId + 1;
+        
+        const user = {
+              id: newId,
+              name: req.body.name,
+              lastname: req.body.lastname,
+              email: req.body.email,
+              usuario: req.body.usuario,
+              pasword: req.body.password
+              
+            };
+
+        users.push(user);
+
+
+        const jsonTxt = JSON.stringify(users, null, 2);
+        fs.writeFileSync(usersFilePath, jsonTxt, "utf-8");
+
+        res.redirect("/user/login" + newId);
+         
+    }
+     
+     
+     
 }
 
 module.exports = userController;
