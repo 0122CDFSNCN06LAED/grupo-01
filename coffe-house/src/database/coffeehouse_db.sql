b3dbf05fd77e6154eba108e3573c2f54c34c42d8
ludmila wagner <lu.wagner91@gmail.com>
2022-06-15 18:31:46 -0300

    agrego un prod a la db

--- coffe-house/src/database/coffeehouse_db.sql
+++ coffe-house/src/database/coffeehouse_db.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 14-06-2022 a las 22:36:17
+-- Tiempo de generación: 15-06-2022 a las 23:30:28
 -- Versión del servidor: 10.4.21-MariaDB
 -- Versión de PHP: 8.0.10
 
@@ -72,7 +72,8 @@ INSERT INTO `grinds_products` (`id`, `id_grind`, `id_product`) VALUES
 (40, 1, 7),
 (41, 3, 7),
 (42, 2, 48),
-(43, 3, 48);
+(43, 3, 48),
+(44, 2, 49);
 
 -- --------------------------------------------------------
 
@@ -99,10 +100,11 @@ INSERT INTO `products` (`id`, `name`, `description`, `price`, `region`, `image`,
 (1, 'Burundi', 'Un café único para paladares exquisitos, un café delicioso. Muy exótico, dulce, con un dulzor particular, acidez cítrica y notas florales que perduran en boca. Presenta un cuerpo sedoso y notas a toffee, pomelo rosado, earl gray y bergamota. Con un tueste', 2500, 'Mutumba', 'product-1650654494013.jpg', 1, 10),
 (3, 'Kenya', 'Kenya es un café de cuerpo medio y, como muchos de los cafés africanos, \n        se transforma en una experiencia única cuando se combina con hielo. \n        Este café se diferencia por su sabor a cítricos que deja entrever toques de pomelo, \n        limó', 2500, 'Muranga', 'product-1650655824942.jpg', 2, 10),
 (4, 'Guatemala', 'Un café dulce y cremoso, con notas a chocolate amargo y avellanas, con una acidez dulce con notas a ciruela. Ideal para espresso y moka italiana. Debido al cuerpo alto y notas dulces es un café que combina muy bien con todo tipo de leche.', 2000, 'Huehuetenango', 'product-1650658479882.jpg', 1, 10),
-(5, 'Colombia', 'Un café dulce y muy fácil de tomar, con notas a chocolate y naranja, perfectamente balanceado. Ideal para espresso y moka italiana: nuestro café recomendado para preparar bebidas con leche.', 2200, 'Tolina', 'product-1650659055982.jpg', 2, 10),
+(5, 'Colombia', 'Un café dulce y muy fácil de tomar, con notas a chocolate y naranja, perfectamente balanceado. Ideal para espresso y moka italiana: nuestro café recomendado para preparar bebidas con leche.', 2200, 'Tolina', 'product-1650658798049.jpg', 2, 10),
 (6, 'Colombia', 'Un café suave, exótico y delicado, con notas a frutilla y cereza, canela, y jazmín. Recomendado para métodos filtrados. Chemex, V60, Katita, Melitta, Clever, etc.', 2400, 'Huila', 'product-1650662381977.jpg', 2, 10),
 (7, 'Sumatra Mandheling', 'El Café Sumatra Mandheling, cultivado en la región centro-oeste cerca de Padang, es conocido por su cuerpo liso y pesado que a veces se describe como almibarado. Los mejores cafés de Sumatra tienen una acidez relativamente baja, pero lo suficiente como pa', 2600, 'Padang', 'product-1650727852178.jpg', 3, 10),
-(48, 'Nicaragua', 'El café de Nicaragua se caracteriza por tener un cuerpo suave y una acidez suave. No obstante, su sabor es muy particular: combina sabores sutiles y dulces, una combinación de nuez, chocolate, frutos rojos y toques de vainilla.', 2100, 'Matagalpa', 'product-1655234338247.jpg', 2, 8);
+(48, 'Nicaragua', 'El café de Nicaragua se caracteriza por tener un cuerpo suave y una acidez suave. No obstante, su sabor es muy particular: combina sabores sutiles y dulces, una combinación de nuez, chocolate, frutos rojos y toques de vainilla.', 2100, 'Matagalpa', 'product-1655234338247.jpg', 2, 8),
+(49, 'Combo Medium Roast', 'Combo compuesto por una selección de tres tipos de cafés de tueste medio.', 4500, 'Colombia / Kenia / Nicaragua', 'product-1655320000357.jpg', 2, 5);
 
 -- --------------------------------------------------------
 
@@ -213,7 +215,8 @@ INSERT INTO `weight_products` (`id`, `id_weight`, `id_product`) VALUES
 (14, 3, 5),
 (15, 2, 6),
 (16, 2, 7),
-(17, 3, 7);
+(17, 3, 7),
+(19, 2, 49);
 
 --
 -- Índices para tablas volcadas
@@ -295,13 +298,13 @@ ALTER TABLE `grinds`
 -- AUTO_INCREMENT de la tabla `grinds_products`
 --
 ALTER TABLE `grinds_products`
-  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=44;
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=45;
 
 --
 -- AUTO_INCREMENT de la tabla `products`
 --
 ALTER TABLE `products`
-  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=49;
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=50;
 
 --
 -- AUTO_INCREMENT de la tabla `products_users`
@@ -337,7 +340,7 @@ ALTER TABLE `weight`
 -- AUTO_INCREMENT de la tabla `weight_products`
 --
 ALTER TABLE `weight_products`
-  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=19;
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=20;
 
 --
 -- Restricciones para tablas volcadas
