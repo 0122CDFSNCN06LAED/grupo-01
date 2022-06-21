03d35971fa51f1e02386339519aef11f9b2e9f80
RubenVen <venruben21@gmail.com>
2022-06-21 17:04:05 -0300

    Modifico rutas

--- coffe-house/src/routes/user.js
+++ coffe-house/src/routes/user.js
@@ -1,7 +1,7 @@
+const path = require("path");
 const express = require("express");
 const userController = require("../controllers/userController");
 const multer = require("multer");
-const path = require("path");
 const validateRegister = require("../middlewares/validateRegisterMiddleware");
 const guestMiddleware = require("../middlewares/guestMiddleware");
 const authMiddleware = require("../middlewares/authMiddleware");
@@ -23,29 +23,28 @@ const storage = multer.diskStorage({
 });
 const uploadFile = multer({ storage });
 
-router.get("/user/login", guestMiddleware, userController.login);
-// router.post('/user/login',userController.loginProcess);
-router.post("/user/login", userSequelizeController.logProcess);
-router.get("/user/register", guestMiddleware, userController.register);
-// router.post("/user/register", uploadFile.single('avatar'),validateRegister, userController.processRegister);
+router.get("/user/login", guestMiddleware, userSequelizeController.login);
+router.post("/user/login", userSequelizeController.loginProcess);
+router.get("/user/register", guestMiddleware, userSequelizeController.register);
 router.post(
   "/user/register",
   uploadFile.single("avatar"),
   validateRegister,
   userSequelizeController.userCreate
 );
-router.get("/user/search", userController.userSearch);
-router.post("/user/search", validateSearch, userSequelizeController.userDetail);
-router.get("/user/profile", authMiddleware, userController.profile);
-router.get("/user/logout", userController.logout);
-router.get("/user/detail/", userController.userDetail);
+router.get("/user/profile", authMiddleware, userSequelizeController.profile);
+router.get("/user/logout", userSequelizeController.logout);
+router.get("/user/detail/:id", userSequelizeController.userDetail);
 router.put(
-  "/user/update/:id",
+  "/user/edit/:id",
   uploadFile.single("avatar"),
   validateSearchUserRegister,
   userSequelizeController.update
 );
-router.get("/user/delete/confirm/", userController.userDeleteConfim);
+router.get(
+  "/user/delete/confirm/:id",
+  userSequelizeController.userDeleteConfim
+);
 router.delete(
   "/user/delete/:id",
   uploadFile.single("avatar"),
