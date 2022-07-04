736ba525bdee0f2a64db6e441f518ec9f3dbbf2e
RubenVen <venruben21@gmail.com>
2022-07-03 21:45:11 -0300

    Modificación de nombre Archivo

---
+++ coffe-house/src/middlewares/validationUserEditMiddleware.js
@@ -0,0 +1,22 @@
+const { body } = require("express-validator");
+
+const validateUserEditRegister = [
+  body("name").notEmpty().withMessage("Tienes que escribir un nombre"),
+  body("lastname").notEmpty().withMessage("Tienes que escribir un apellido"),
+  body("username")
+    .notEmpty()
+    .withMessage("Tienes que escribir un nombre de usuario"),
+  body("email")
+    .notEmpty()
+    .withMessage("Tienes que escribir un correo electrónico")
+    .bail()
+    .isEmail()
+    .withMessage("Debes escribir un formato de correo válido"),
+  body("password")
+    .notEmpty()
+    .withMessage("Tienes que escribir una contraseña")
+    .bail()
+    .isLength({ min: 6 }),
+];
+
+module.exports = validateUserEditRegister;
