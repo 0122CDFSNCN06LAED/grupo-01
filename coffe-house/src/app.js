ca12b8928ba95f9c0daf8570e14019500c8d280a
RubenVen <venruben21@gmail.com>
2022-07-14 21:03:03 -0300

    Agrego ruta userListApiRouter

--- coffe-house/src/app.js
+++ coffe-house/src/app.js
@@ -1,29 +1,31 @@
 const express = require("express");
 const app = express();
 const path = require("path");
-const rutaHome = require ('./routes/home');
-const rutaUser = require ('./routes/user');   
-const rutaProduct = require('./routes/product');
-const methodOverride = require ('method-override');
-const session = require('express-session');
-const userLoggedMiddleware = require('./middlewares/userLoggedMiddleware');
-const cookies = require('cookie-parser');
+const rutaHome = require("./routes/home");
+const rutaUser = require("./routes/user");
+const rutaProduct = require("./routes/product");
+const methodOverride = require("method-override");
+const session = require("express-session");
+const userLoggedMiddleware = require("./middlewares/userLoggedMiddleware");
+const cookies = require("cookie-parser");
 const adminMiddleware = require("./middlewares/adminMiddleware");
+const userListApiRouter = require("./routes/api/UserListApiRouter");
 
-
-app.use(session({
-  secret: "Shh, it's a secret",
-  resave: false,
-  saveUninitialized: false,
-}));
+app.use(
+  session({
+    secret: "Shh, it's a secret",
+    resave: false,
+    saveUninitialized: false,
+  })
+);
 app.use(cookies());
-app.use(userLoggedMiddleware); 
-app.use(adminMiddleware) 
-app.use(methodOverride('_method'))
-app.use(express.urlencoded({extended:false}));
-app.use(express.json())
- 
-app.set('view engine', 'ejs'); 
+app.use(userLoggedMiddleware);
+app.use(adminMiddleware);
+app.use(methodOverride("_method"));
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+
+app.set("view engine", "ejs");
 app.set("views", path.resolve(__dirname, "./views"));
 
 //le damos acceso a carpeta public
@@ -36,7 +38,6 @@ app.listen(3000, () => {
 //RUTAS NUEVAS
 app.use(rutaHome);
 app.use(rutaUser);
+app.use("/api", userListApiRouter);
 
 app.use(rutaProduct);
-
-
