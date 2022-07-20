47d5b63550b852b05f725c9b7d2ff8112642efc0
RubenVen <venruben21@gmail.com>
2022-07-19 23:57:41 -0300

    Borro linea productSequelizeController,js

--- coffe-house/src/controllers/productSequelizeController.js
+++ coffe-house/src/controllers/productSequelizeController.js
@@ -1,4 +1,4 @@
-const { validationResult, body } = require('express-validator');
+const { validationResult, body } = require("express-validator");
 const db = require("../database/models");
 const { Op } = require("sequelize");
 
@@ -8,8 +8,6 @@ const WeightProducts = db.WeightProducts;
 const Grind = db.Grinds;
 const Weight = db.Weight;
 const ProductCategory = db.ProductCategories;
-
-
 
 const productSequelizeController = {
   list: async (req, res) => {
@@ -46,10 +44,9 @@ const productSequelizeController = {
       });
     } catch (err) {
       console.log(err);
-      res.render('products/product-error');
+      res.render("products/product-error");
     }
   },
-
 
   store: async (req, res) => {
     let erroresValidacion = validationResult(req);
@@ -60,18 +57,14 @@ const productSequelizeController = {
     ]);
 
     if (!erroresValidacion.isEmpty()) {
-
-
       return res.render("products/product-create", {
         errors: erroresValidacion.mapped(),
         weight: weight,
         grinds: grinds,
         productCategory: productCategory,
         oldData: req.body,
-        oldDataFile: req.file ? req.file.filename : ""
-      })
-    }
-    else {
+      });
+    } else {
       try {
         const newProduct = await Products.create({
           name: req.body.name,
@@ -116,31 +109,26 @@ const productSequelizeController = {
         res.redirect("/product/detail/" + newProduct.id);
       } catch (err) {
         console.log(err);
-        res.render('products/product-error');
+        res.render("products/product-error");
       }
     }
   },
 
   product: async (req, res) => {
-
     try {
-      const product = await Products.findByPk(req.params.id,
-        {
-          include: [
-            { all: true }]
-        });
+      const product = await Products.findByPk(req.params.id, {
+        include: [{ all: true }],
+      });
 
       res.render("products/product-detail", {
-        product
+        product,
       });
     } catch (err) {
       console.error(err);
       res.render("products/product-error");
     }
-
   },
   edit: async (req, res) => {
-
     const id = req.params.id;
 
     const [product, dbweights, dbgrinds, productCategory] = await Promise.all([
@@ -162,7 +150,6 @@ const productSequelizeController = {
     });
   },
   update: async (req, res) => {
-
     let erroresValidacion = validationResult(req);
     const id = req.params.id;
 
@@ -176,7 +163,6 @@ const productSequelizeController = {
     const weightOfThisProduct = product.weight;
 
     if (!erroresValidacion.isEmpty()) {
-
       res.render("products/product-edit-seq", {
         errors: erroresValidacion.mapped(),
         product,
@@ -187,29 +173,29 @@ const productSequelizeController = {
         productCategory,
       });
 
-
       //res.send(erroresValidacion)
     } else {
-
-
       try {
         const oldProduct = await Products.findByPk(req.params.id);
-        const editProduct = await Products.update({
-          name: req.body.name,
-          region: req.body.region,
-          description: req.body.description,
-          image: req.file ? req.file.filename : oldProduct.image,
-          price: req.body.price,
-          stock: req.body.stock,
-          category_id: req.body.category,
-        }, {
-          where: { id: oldProduct.id }
-        })
+        const editProduct = await Products.update(
+          {
+            name: req.body.name,
+            region: req.body.region,
+            description: req.body.description,
+            image: req.file ? req.file.filename : oldProduct.image,
+            price: req.body.price,
+            stock: req.body.stock,
+            category_id: req.body.category,
+          },
+          {
+            where: { id: oldProduct.id },
+          }
+        );
 
         const oldGrinds = await GrindsProducts.destroy({
           where: {
             id_product: oldProduct.id,
-          }
+          },
         });
 
         const newGrinds = req.body.grind;
@@ -230,7 +216,7 @@ const productSequelizeController = {
         const oldWeights = await WeightProducts.destroy({
           where: {
             id_product: oldProduct.id,
-          }
+          },
         });
         const newWeights = req.body.weight;
         if (newWeights.length > 1) {
@@ -238,28 +224,22 @@ const productSequelizeController = {
             WeightProducts.create({
               id_weight: weight,
               id_product: oldProduct.id,
-            })
-
+            });
           });
         } else {
           WeightProducts.create({
             id_weight: newWeights,
             id_product: oldProduct.id,
-          })
+          });
         }
-
-
-
 
         res.redirect("/product/detail/" + oldProduct.id);
       } catch (err) {
         console.log(err);
-        res.render('products/product-error');
+        res.render("products/product-error");
       }
     }
   },
-
-
 
   delete: async (req, res) => {
     let id = req.params.id;
@@ -270,10 +250,12 @@ const productSequelizeController = {
   destroy: async (req, res) => {
     try {
       let id = req.params.id;
-      const product = await Products.destroy({ where: { id: id }, force: true }) // force: true es para asegurar que se ejecute la acción
+      const product = await Products.destroy({
+        where: { id: id },
+        force: true,
+      }); // force: true es para asegurar que se ejecute la acción
 
       return res.redirect("/product");
-
     } catch (err) {
       console.error(err);
       res.render("products/product-error");
@@ -284,27 +266,23 @@ const productSequelizeController = {
       where: {
         [Op.or]: {
           name: {
-            [Op.like]: '%' + req.query.search + '%'
+            [Op.like]: "%" + req.query.search + "%",
           },
           description: {
-            [Op.like]: '%' + req.query.search + '%'
+            [Op.like]: "%" + req.query.search + "%",
           },
           region: {
-            [Op.like]: '%' + req.query.search + '%'
+            [Op.like]: "%" + req.query.search + "%",
           },
-
-        }
-
-      }
-    })
-      .then(function (products) {
-        res.render("products/search", { products: products })
-      })
+        },
+      },
+    }).then(function (products) {
+      res.render("products/search", { products: products });
+    });
   },
   error: (req, res) => {
-
-    res.render('products/product-error');
-  }
+    res.render("products/product-error");
+  },
 };
 
-module.exports = productSequelizeController
+module.exports = productSequelizeController;
