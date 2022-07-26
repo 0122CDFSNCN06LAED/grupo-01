d66b92f97ecc72de36c6dc7adf5d8a07078208fe
RubenVen <venruben21@gmail.com>
2022-07-26 18:41:57 -0300

    Agrego ruta para mostar todos los productos

--- coffe-house/src/routes/api/ProductApiRouter.js
+++ coffe-house/src/routes/api/ProductApiRouter.js
@@ -4,10 +4,12 @@ const {
 } = require("../../controllers/api/CategoriesProdApiController");
 const {
   productsList,
+  allProducts,
 } = require("../../controllers/api/ProductListApiController");
 const router = Router();
 
 router.get("/table-products", productsList);
 router.get("/categories-products", ProductCategories);
+router.get("/products", allProducts);
 
 module.exports = router;
