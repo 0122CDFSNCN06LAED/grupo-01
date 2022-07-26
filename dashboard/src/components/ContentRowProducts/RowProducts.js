175cf8cfc74fdfa68a2f1e8e737ef2f98486ec1f
RubenVen <venruben21@gmail.com>
2022-07-25 22:55:14 -0300

    Agrego RowProducts.js en dashboard

---
+++ dashboard/src/components/ContentRowProducts/RowProducts.js
@@ -0,0 +1,54 @@
+import React from "react";
+import PropTypes from "prop-types";
+
+function RowProducts(props) {
+  return (
+    <div className="col-md-4 mb-4">
+      <div className={`card border-left-${props.color} shadow h-100 py-2`}>
+        <div className="card-body">
+          <div className="row no-gutters align-items-center">
+            <div className="col mr-2">
+              <div
+                className={`text-xs font-weight-bold text-${props.color} text-uppercase mb-1`}
+              >
+                {props.title}
+              </div>
+              <div className="h5 mb-0 font-weight-bold text-gray-800">
+                {props.value}
+              </div>
+            </div>
+            <div className="col-auto">
+              <i className={`fas ${props.icon} fa-2x text-gray-300`}></i>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+RowProducts.defaultProps = {
+  title: "Sin titulo",
+  value: "Sin valor",
+  color: "primary",
+  icon: "fa-dollar",
+};
+
+RowProducts.propTypes = {
+  title: PropTypes.string.isRequired,
+  value: PropTypes.number.isRequired,
+  color: PropTypes.oneOf([
+    "primary",
+    "secondary",
+    "success",
+    "danger",
+    "warning",
+    "info",
+    "light",
+    "dark",
+    "muted",
+    "white",
+  ]).isRequired,
+  icon: PropTypes.string,
+};
+
+export default RowProducts;
