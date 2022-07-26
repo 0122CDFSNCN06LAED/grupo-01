df9cb05d4415f4672a9ed7c91a2cda8f36cc78a4
RubenVen <venruben21@gmail.com>
2022-07-25 22:57:47 -0300

    Agrego ContentWrapper.js en dashboard

---
+++ dashboard/src/components/ContentWrapper/ContentWrapper.js
@@ -0,0 +1,81 @@
+import { Component } from "react";
+import TopBar from "../TopBar/TopBar";
+import Footer from "../Footer/Footer";
+import { Switch, Route } from "react-router-dom";
+import ContentRowTop from "../ContentRowTop/ContenRowTop";
+import LastsProductsInDB from "../LastsProductsInDB/LastsproductsInDB";
+// import ContentCategoriesInDb from "../ContentProductsInDb/ContentProductsInDb";
+import TableProductsList from "../TableProcuts/TableProducts";
+import TableUsersList from "../TableUsers/TableUsers";
+import ContentProductsInDB from "../ContentProductsInDb/ContentProductsInDb";
+
+class ContentWrapper extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      products: null,
+    };
+  }
+  render() {
+    if (!this.state.products) {
+      return "Cargando...";
+    }
+    return (
+      <>
+        {/* <!-- Content Wrapper --> */}
+        <div id="content-wrapper" className="d-flex flex-column">
+          {/* <!-- Main Content --> */}
+          <div id="content">
+            <TopBar />
+
+            <Switch>
+              <Route path="/" exact={true} component={ContentRowTop} />
+
+              <Route
+                path="/products"
+                exact={true}
+                component={ContentProductsInDB}
+              />
+              <Route path="/last-products" exact={true}>
+                <LastsProductsInDB
+                  key={this.state.products.id}
+                  title={"Lo último de Coffee House"}
+                  description={this.state.products.description}
+                />
+              </Route>
+              <Route
+                path="/table-products"
+                exact={true}
+                component={TableProductsList}
+              />
+              <Route
+                path="/table-users"
+                exact={true}
+                component={TableUsersList}
+              />
+            </Switch>
+          </div>
+          {/* <!-- End of MainContent --> */}
+          <Footer />
+        </div>
+        {/* <!-- End of Content Wrapper --> */}
+      </>
+    );
+  }
+  async componentDidMount() {
+    const products = await fetch(
+      "http://localhost:3002/api/table-products"
+    ).catch((error) => {
+      console.log(error);
+    });
+    const response = await products.json();
+
+    this.setState({
+      products: response.data.reduce((acc, value) => {
+        return value;
+      }),
+    });
+  }
+}
+
+export default ContentWrapper;
