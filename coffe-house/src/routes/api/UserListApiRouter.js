c9a7836d0dcd29f13cad1af5b1f9bcd8e6e7cce4
RubenVen <venruben21@gmail.com>
2022-07-28 19:31:22 -0300

    Agrego ruta users

--- coffe-house/src/routes/api/UserListApiRouter.js
+++ coffe-house/src/routes/api/UserListApiRouter.js
@@ -2,9 +2,11 @@ const { Router } = require("express");
 const {
   userListEmails,
   userList,
+  allUsers,
 } = require("../../controllers/api/UserListApiController");
 const router = Router();
 
 router.get("/user/list", userListEmails);
 router.get("/table-users", userList);
+router.get("/users", allUsers);
 module.exports = router;
