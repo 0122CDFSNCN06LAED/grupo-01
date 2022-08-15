2af68e6602fc956cda4416e9844a34bc587a90d9
RubenVen <venruben21@gmail.com>
2022-08-15 20:57:14 -0300

    Agrego div y clases en TableUsers.js

--- dashboard/src/components/TableUsers/TableUsers.js
+++ dashboard/src/components/TableUsers/TableUsers.js
@@ -39,44 +39,49 @@ class TableUsersList extends Component {
 
   render() {
     return (
-      <>
+      <div className="col-lg-12 mb-4">
         <div className="d-sm-flex align-items-center justify-content-between mb-4">
           <h5 className="h3 mb-0 text-gray-800">All the users in Database</h5>
         </div>
-        <table className="table">
-          <thead>
-            <tr>
-              <th scope="col">Id</th>
-              <th scope="col">Name</th>
-              <th scope="col">Lastname</th>
-              <th scope="col">Email</th>
-              <th scope="col">Username</th>
-              <th scope="col">Avatar</th>
-              <th scope="col">User category</th>
-            </tr>
-          </thead>
-          <tbody>
-            {/* ------ Aqui van los datos de la tabla products ------*/}
-            {this.state.users.map((user) => {
-              return (
-                <tr key={user.id}>
-                  <th scope="row">{user.id}</th>
-                  <td>{user.name}</td>
-                  <td>{user.lastname}</td>
-                  <td>{user.email}</td>
-                  <td>{user.username}</td>
-                  <td><img
-              className="img-fluid px-3 px-sm-4 mt-3 mb-4"
-              style={{ width: "8rem" }}
-              src= {user.avatar}
-              alt={user.avatar}/></td>
-              <td>{user.category}</td>
-                </tr>
-              );
-            })}
-            {/*------ Fin de los datos de la tabla products-------- */}
-          </tbody>
-        </table>
+        <div className="table-responsive">
+          <table className="table table-hover">
+            <thead>
+              <tr>
+                <th scope="col">Id</th>
+                <th scope="col">Name</th>
+                <th scope="col">Lastname</th>
+                <th scope="col">Email</th>
+                <th scope="col">Username</th>
+                <th scope="col">Avatar</th>
+                <th scope="col">User category</th>
+              </tr>
+            </thead>
+            <tbody>
+              {/* ------ Aqui van los datos de la tabla products ------*/}
+              {this.state.users.map((user) => {
+                return (
+                  <tr key={user.id}>
+                    <th scope="row">{user.id}</th>
+                    <td>{user.name}</td>
+                    <td>{user.lastname}</td>
+                    <td>{user.email}</td>
+                    <td>{user.username}</td>
+                    <td>
+                      <img
+                        className="img-fluid px-3 px-sm-4 mt-3 mb-4"
+                        style={{ width: "8rem" }}
+                        src={user.avatar}
+                        alt={user.avatar}
+                      />
+                    </td>
+                    <td>{user.category}</td>
+                  </tr>
+                );
+              })}
+              {/*------ Fin de los datos de la tabla products-------- */}
+            </tbody>
+          </table>
+        </div>
         <div className="d-flex justify-content-center">
           <button
             className="btn btn-primary"
@@ -102,7 +107,7 @@ class TableUsersList extends Component {
             <i className="fas fa-chevron-right"></i>
           </button>
         </div>
-      </>
+      </div>
     );
   }
 }
