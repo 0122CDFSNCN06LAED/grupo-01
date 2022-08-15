9b4aa40828c6d73c6b92598d5548efe142812944
RubenVen <venruben21@gmail.com>
2022-08-15 20:56:07 -0300

    Agrego div y clases

--- dashboard/src/components/TableProcuts/TableProducts.js
+++ dashboard/src/components/TableProcuts/TableProducts.js
@@ -39,49 +39,53 @@ class TableProductsList extends Component {
 
   render() {
     return (
-      <>
+      <div className="col-lg-12 mb-4">
         <div className="d-sm-flex align-items-center justify-content-between mb-4">
           <h5 className="h3 mb-0 text-gray-800">
             All the products in Database
           </h5>
         </div>
-        <table className="table">
-          <thead>
-            <tr>
-              <th scope="col">Id</th>
-              <th scope="col">Name</th>
-              <th scope="col">Description</th>
-              <th scope="col">Price $</th>
-              <th scope="col">Region</th>
-              <th scope="col">Image</th>
-              <th scope="col">Category</th>
-              <th scope="col">Stock</th>
-            </tr>
-          </thead>
-          <tbody>
-            {/* ------ Aqui van los datos de la tabla products ------*/}
-            {this.state.products.map((product) => {
-              return (
-                <tr key={product.id}>
-                  <th scope="row">{product.id}</th>
-                  <td>{product.name}</td>
-                  <td>{product.description}</td>
-                  <td>{product.price}</td>
-                  <td>{product.region}</td>
-                  <td><img
-              className="img-fluid px-3 px-sm-4 mt-3 mb-4"
-              style={{ width: "110rem" }}
-              src= {product.image}
-              alt={product.image}
-            /></td>
-                  <td>{product.category}</td>
-                  <td>{product.stock}</td>
-                </tr>
-              );
-            })}
-            {/*------ Fin de los datos de la tabla products-------- */}
-          </tbody>
-        </table>
+        <div className="table-responsive">
+          <table className="table table-hover">
+            <thead>
+              <tr>
+                <th scope="col">Id</th>
+                <th scope="col">Name</th>
+                <th scope="col">Description</th>
+                <th scope="col">Price$</th>
+                <th scope="col">Region</th>
+                <th scope="col">Image</th>
+                <th scope="col">Category</th>
+                <th scope="col">Stock</th>
+              </tr>
+            </thead>
+            <tbody>
+              {/* ------ Aqui van los datos de la tabla products ------*/}
+              {this.state.products.map((product) => {
+                return (
+                  <tr key={product.id}>
+                    <th scope="row">{product.id}</th>
+                    <td>{product.name}</td>
+                    <td>{product.description}</td>
+                    <td>{product.price}</td>
+                    <td>{product.region}</td>
+                    <td>
+                      <img
+                        className="img-fluid px-3 px-sm-4 mt-3 mb-4"
+                        style={{ width: "110rem" }}
+                        src={product.image}
+                        alt={product.image}
+                      />
+                    </td>
+                    <td>{product.category}</td>
+                    <td>{product.stock}</td>
+                  </tr>
+                );
+              })}
+              {/*------ Fin de los datos de la tabla products-------- */}
+            </tbody>
+          </table>
+        </div>
         <div className="d-flex justify-content-center">
           <button
             className="btn btn-primary"
@@ -107,7 +111,7 @@ class TableProductsList extends Component {
             <i className="fas fa-chevron-right"></i>
           </button>
         </div>
-      </>
+      </div>
     );
   }
 }
