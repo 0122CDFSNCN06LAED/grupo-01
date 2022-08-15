ff38029958efd2b7049a38b5a7a0b56a0bf68ab0
RubenVen <venruben21@gmail.com>
2022-08-15 20:43:18 -0300

    Agrego ruta para login dashboard

--- coffe-house/src/routes/api/UserListApiRouter.js
+++ coffe-house/src/routes/api/UserListApiRouter.js
@@ -3,10 +3,13 @@ const {
   userListEmails,
   userList,
   allUsers,
+  userLogged,
 } = require("../../controllers/api/UserListApiController");
 const router = Router();
 
 router.get("/user/list", userListEmails);
 router.get("/table-users", userList);
 router.get("/users", allUsers);
+router.get("/user-logged", userLogged);
+
 module.exports = router;
