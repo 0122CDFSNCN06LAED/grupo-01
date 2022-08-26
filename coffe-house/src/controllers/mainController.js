48c733ced46f867090717be22a4916ec19e60f86
RubenVen <venruben21@gmail.com>
2022-08-26 17:11:32 -0300

    Modifico método home para mostrar productos con stock mayores a cero.

--- coffe-house/src/controllers/mainController.js
+++ coffe-house/src/controllers/mainController.js
@@ -1,34 +1,36 @@
-const path = require ("path");
+const path = require("path");
+const { Op } = require("sequelize");
 const db = require("../database/models");
 const Products = db.Products;
 
 const mainController = {
-    home: async (req, res) => {
-        const products = await Products.findAll({
-            include: [
-              {
-                association: "productCategory",
-              },
-            ],
-          });
-      
-          const blonde = products.filter((p) => p.productCategory.type == "Blonde");
-          const medium = products.filter((p) => p.productCategory.type == "Medium");
-          const dark = products.filter((p) => p.productCategory.type == "Dark");
-        res.render("home", {
-            blonde: blonde,
-            medium: medium,
-            dark: dark,
-          });
-    },
-    nuestrosCafes: (req,res) =>{
-      res.render("nuestros-cafes")
-    },
-    contacto: (req,res) =>{
-      res.render("contacto")
-    }
-   
-}
+  home: async (req, res) => {
+    const emailUserLogged = req.session.emailUserLogged;
 
+    const products = await Products.findAll({
+      include: [
+        {
+          association: "productCategory",
+        },
+      ],
+      where: emailUserLogged ? "" : { stock: { [Op.gt]: 0 } },
+    });
 
-    module.exports= mainController
+    const blonde = products.filter((p) => p.productCategory.type == "Blonde");
+    const medium = products.filter((p) => p.productCategory.type == "Medium");
+    const dark = products.filter((p) => p.productCategory.type == "Dark");
+    res.render("home", {
+      blonde: blonde,
+      medium: medium,
+      dark: dark,
+    });
+  },
+  nuestrosCafes: (req, res) => {
+    res.render("nuestros-cafes");
+  },
+  contacto: (req, res) => {
+    res.render("contacto");
+  },
+};
+
+module.exports = mainController;
