d245b372bd47be1215dd862b54b3674b66ad3c2f
RubenVen <venruben21@gmail.com>
2022-08-26 17:15:26 -0300

    Modifico adminMiddelware,js agrego session emailUserLogged

--- coffe-house/src/middlewares/adminMiddleware.js
+++ coffe-house/src/middlewares/adminMiddleware.js
@@ -1,21 +1,16 @@
+function adminMiddleware(req, res, next) {
+  if (req.session.userLogged) {
+    let emailUserLogged = req.session.userLogged.email;
+    const mailAdmin = "@coffeehouse.com";
 
+    if (emailUserLogged.endsWith(mailAdmin)) {
+      res.locals.isAdmin = req.session.userLogged;
+      req.session.emailUserLogged = true;
 
+      // console.log(res.locals.isAdmin);
+    }
+  }
 
-function adminMiddleware (req, res, next) {
-    if(req.session.userLogged) {
-        let emailUserLogged = req.session.userLogged.email;
-        const mailAdmin= "@coffeehouse.com"
-        if(emailUserLogged.endsWith(mailAdmin)){
-           res.locals.isAdmin = req.session.userLogged
-             
-            console.log(res.locals.isAdmin)
-        }
-        
-    } 
-	
-  
-
-    
-    next()
+  next();
 }
-module.exports=adminMiddleware
+module.exports = adminMiddleware;
