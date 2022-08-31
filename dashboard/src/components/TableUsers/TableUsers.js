24f3d9609cea23d2a5223205c62e05cac87880f6
RubenVen <venruben21@gmail.com>
2022-08-31 20:42:31 -0300

    Modifico TableUsers.js, dashboard, responsive.

--- dashboard/src/components/TableUsers/TableUsers.js
+++ dashboard/src/components/TableUsers/TableUsers.js
@@ -1,4 +1,7 @@
 import React, { Component } from "react";
+import { Table, Thead, Tbody, Tr, Th, Td } from "react-super-responsive-table";
+import "react-super-responsive-table/dist/SuperResponsiveTableStyle.css";
+import "../../assets/css/table.css";
 
 class TableUsersList extends Component {
   constructor(props) {
@@ -41,46 +44,48 @@ class TableUsersList extends Component {
     return (
       <div className="col-lg-12 mb-4">
         <div className="d-sm-flex align-items-center justify-content-between mb-4">
-          <h5 className="h3 mb-0 text-gray-800">All the users in Database</h5>
+          <h5 className="h3 mb-0 text-gray-800 h3-table">
+            All the users in Database
+          </h5>
         </div>
         <div className="table-responsive">
-          <table className="table table-hover">
-            <thead>
-              <tr>
-                <th scope="col">Id</th>
-                <th scope="col">Name</th>
-                <th scope="col">Lastname</th>
-                <th scope="col">Email</th>
-                <th scope="col">Username</th>
-                <th scope="col">Avatar</th>
-                <th scope="col">User category</th>
-              </tr>
-            </thead>
-            <tbody>
+          <Table className="table table-hover table-responsive-sm table-users-products table-md">
+            <Thead>
+              <Tr className="tr-table-margin-bottom">
+                <Th scope="col">Id</Th>
+                <Th scope="col">Name</Th>
+                <Th scope="col">Lastname</Th>
+                <Th scope="col">Email</Th>
+                <Th scope="col">Username</Th>
+                <Th scope="col">Avatar</Th>
+                <Th scope="col">User category</Th>
+              </Tr>
+            </Thead>
+            <Tbody>
               {/* ------ Aqui van los datos de la tabla products ------*/}
               {this.state.users.map((user) => {
                 return (
-                  <tr key={user.id}>
-                    <th scope="row">{user.id}</th>
-                    <td>{user.name}</td>
-                    <td>{user.lastname}</td>
-                    <td>{user.email}</td>
-                    <td>{user.username}</td>
-                    <td>
+                  <Tr key={user.id} className="tr-table-margin-bottom">
+                    <Td scope="row">{user.id}</Td>
+                    <Td>{user.name}</Td>
+                    <Td>{user.lastname}</Td>
+                    <Td>{user.email}</Td>
+                    <Td>{user.username}</Td>
+                    <Td>
                       <img
-                        className="img-fluid px-3 px-sm-4 mt-3 mb-4"
+                        className="img-fluid px-3 px-sm-4 mt-3 mb-4 img-padding-cero"
                         style={{ width: "8rem" }}
                         src={user.avatar}
                         alt={user.avatar}
                       />
-                    </td>
-                    <td>{user.category}</td>
-                  </tr>
+                    </Td>
+                    <Td>{user.category}</Td>
+                  </Tr>
                 );
               })}
               {/*------ Fin de los datos de la tabla products-------- */}
-            </tbody>
-          </table>
+            </Tbody>
+          </Table>
         </div>
         <div className="d-flex justify-content-center">
           <button
